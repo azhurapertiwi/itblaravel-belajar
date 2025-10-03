af2e32462534f755201a8eef662a6724ba8613e6
agoengBani <agungsyabani99@gmail.com>
2025-10-03 18:05:37 +0700

    menggunakan controller dan tampil data di blade

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -1,1 +1,21 @@
-ini dosen 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ini halaman dosen</h1>
+    @foreach ($data as $dosen)
+        <p>{{ $dosen['nama'] }}</p>
+        <p>{{ $dosen['alamat'] }}</p>
+        <p>{{ $dosen['hp'] }}</p>
+    @endforeach
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DosenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/dosen', function () {
     return view('dosen');
 });
+
+Route::get('/dosen', [DosenController::class, 'index']);
